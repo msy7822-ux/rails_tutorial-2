c5a3e84f1be16fcdcacf2dd6c460b83687b274f7
msy <msy780022@gmail.com>
2020-06-24 00:53:13 +0000

    add validates for content

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+  resources :microposts
+  resources :users
+  # scaffoldのモデルを作成した時点で自動生成される
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
-  root "application#hello"
+  root "users#index"
   # ここは、コントローラー名＃アクション名で記述
   
 end
